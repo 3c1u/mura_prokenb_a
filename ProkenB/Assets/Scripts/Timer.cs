56858d456e6c018e97dd7a537c4eef7b213074de
Yuta Nagai <67227640+nayuta-ai@users.noreply.github.com>
2020-10-19 10:57:27 +0900

    Update Timer.cs

--- ProkenB/Assets/Scripts/Timer.cs
+++ ProkenB/Assets/Scripts/Timer.cs
@@ -22,21 +22,26 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(countdown>=0){
+        if(countdown>=0)
+	{
             countdown-=Time.deltaTime;
             count=(int)countdown;
             CountText.text=count.ToString();
         }
-        if(countdown<=0){
+        if(countdown<=0)
+	{
             CountText.text="";
-            if(Goal.goal==false){
+            if(Goal.goal==false)
+	    {
                 seconds+=Time.deltaTime;
             }
-            if(seconds >= 60f) {
+            if(seconds >= 60f)
+	    {
                 minute++;
-			    seconds = seconds - 60;
+		seconds = seconds - 60;
             }
-			if((int)seconds != (int)oldSeconds) {
+	    if((int)seconds != (int)oldSeconds)
+	    {
                 timerText.text = minute.ToString("00") + ":" + ((int) seconds).ToString ("00");
             }
             oldSeconds = seconds;
