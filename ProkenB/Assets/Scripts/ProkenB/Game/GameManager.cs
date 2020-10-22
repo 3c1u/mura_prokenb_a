1a523596e5af21d30bfe8e24d6b2c4979d7e0d3d
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-10-22 22:25:21 +0900

    Add classes

---
+++ ProkenB/Assets/Scripts/ProkenB/Game/GameManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ProkenB.Game
+{
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+}
---
+++ ProkenB/Assets/Scripts/ProkenB/Model/PlayerModel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UniRx;
+using System;
+
+namespace ProkenB.Model
+{
+    public class PlayerModel
+    {
+        private ReactiveProperty<Vector3> m_position = new ReactiveProperty<Vector3>(new Vector3());
+
+        /// <summary>
+        /// プレイヤーの位置
+        /// </summary>
+        public Vector3 Position
+        {
+            get => m_position.Value;
+            set => m_position.SetValueAndForceNotify(value);
+        }
+
+        /// <summary>
+        /// プレイヤー位置のObservableオブジェクト．
+        /// </summary>
+        public IObservable<Vector3> PositionAsObservable => m_position.AsObservable();
+
+        private ReactiveProperty<float?> m_goalTime = new ReactiveProperty<float?>(null);
+
+        /// <summary>
+        /// ゴールした時間．`null`ならまた未達成．
+        /// </summary>
+        public float? GoalTime
+        {
+            get => m_goalTime.Value;
+            set => m_goalTime.SetValueAndForceNotify(value);
+        }
+
+        /// <summary>
+        /// ゴールしたかどうか．
+        /// </summary>
+        public bool HasReachedGoal => m_goalTime.Value != null;
+    }
+}
