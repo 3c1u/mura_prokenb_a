49a043d33a9fbee259459df1230473b399eab61e
nayuta-ai <nayuta723@gmail.com>
2020-10-25 16:16:37 +0900

    可視化Voice,Breath

---
+++ ProkenB/Assets/Scripts/VisualSensor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ProkenB.Game;
+
+public class VisualSensor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text fftText;
+    public Text voiceText;
+    public Text breathText;
+
+    void Start()
+    {
+        fftText.text = "FFT:None";
+        voiceText.text="Voice:None";
+        breathText.text="Breath:None";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var breath = GameManager.Instance.Detector.Breath;
+        var voice = GameManager.Instance.Detector.Voice;
+        fftText.text="";
+        voiceText.text="Voice:"+voice;
+        breathText.text="Breath:"+breath;
+    }
+}
