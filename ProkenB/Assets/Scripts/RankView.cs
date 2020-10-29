0d2c14e1ff0eb50bbed5be3e5273eb1130267b1c
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-10-29 21:15:32 +0900

    Fix ranking mechanism

--- ProkenB/Assets/Scripts/RankView.cs
+++ ProkenB/Assets/Scripts/RankView.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using ProkenB.Game;
@@ -7,15 +9,10 @@ using ProkenB.Model;
 
 public class RankView : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Text rankText;
-    Vector3 tmp1 = new Vector3(0.0f, 0.0f, 0.0f);
-    Vector3 tmp2 = new Vector3(0.0f, 0.0f, 0.0f);
-    Vector3 tmp3 = new Vector3(0.0f, 0.0f, 0.0f);
-    // Goal Objectの所在が分からなかったので座標で宣言しておく
-    Vector3 goalPosition = new Vector3(0.0f, 0.0f, 450.0f);
-    public float relativepass1, relativepass2, relativepass3;
-    public int rank;
+    Vector3 m_goalPosition = new Vector3(0.0f, 0.0f, 450.0f);
+
+    public int rank = 0;
 
     void Start()
     {
@@ -25,36 +22,16 @@ public class RankView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var player = GameManager.Instance.Model.Players;
-        var local_player = GameManager.Instance.Model.LocalPlayer;
-        PlayerModel Player1 = player[0];
-        PlayerModel Player2 = player[1];
-        PlayerModel Player3 = player[2];
-        tmp1 = Player1.Position;
-        tmp2 = Player2.Position;
-        tmp3 = Player3.Position;
-        relativepass1 = goalPosition.z - tmp1.z;
-        relativepass2 = goalPosition.z - tmp2.z;
-        relativepass3 = goalPosition.z - tmp3.z;
-        var relativepass = goalPosition.z - local_player.Position.z;
-        if (relativepass == relativepass1)
-        {
-            relativepass = relativepass1;
-        }
-        if (relativepass == relativepass2)
-        {
-            relativepass = relativepass2;
-        }
-        if (relativepass == relativepass3)
-        {
-            relativepass = relativepass3;
-        }
-        float[] List = { relativepass1, relativepass2, relativepass3 };
-        var list = new List<float>();
-        list.AddRange(list);
-        list.Sort();
-        list.Reverse();
-        rank = list.IndexOf(relativepass) + 1;
-        rankText.text = rank + "位";
+        var players = GameManager.Instance.Model.Players;
+        var localPlayer = GameManager.Instance.Model.LocalPlayer;
+
+        var topPlayers =
+        players.OrderBy(p => p.GoalTime ?? float.PositiveInfinity)
+            .ThenBy(p => (p.Position - m_goalPosition).magnitude)
+            .ToList();
+
+        rank = 1 + topPlayers.IndexOf(localPlayer);
+
+        rankText.text = $"{rank} 位";
     }
 }
